aaa3c25704672278d548ea567eecd7a89f6da9c8
marcondercin <marcondercin@gmail.com>
2021-09-02 10:35:55 -0700

    props reference in Track.js

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,7 +4,7 @@ import './Track.css';
 class Track extends React.Component {
   
   renderAction() {
-    const text = isRemoval ? '-' : '+';
+    const text = this.props.isRemoval ? '-' : '+';
     return <button className="Track-action">{text}</button>;
   }
 
@@ -12,10 +12,10 @@ class Track extends React.Component {
     return (
       <div className="Track">
         <div className="Track-information">
-          {/* <h3><!-- track name will go here --></h3> */}
-          {/* <p><!-- track artist will go here--> | <!-- track album will go here --></p> */}
+          <h3>track name</h3>
+          <p>track artist | track album</p>
         </div>
-        <button className="Track-action">{/*<!-- + or - will go here -->*/}</button>
+        {this.renderAction()}
       </div>
     )
   }
