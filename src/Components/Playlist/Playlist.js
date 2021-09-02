195d711cefd1742128665dd71a65e0fd436315fb
marcondercin <marcondercin@gmail.com>
2021-09-02 12:16:52 -0700

    Pass name and tracks props from playlist to tracklist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -7,7 +7,7 @@ class Playlist extends React.Component {
     return (
       <div className="Playlist">
         <input value="New Playlist"/>
-        {/* <TrackList /> */}
+        <TrackList playlistName={this.props.playlistName} playlistTracks={this.props.playlistTracks}/>
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     )
