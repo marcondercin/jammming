d233fe43d9b658f94e8026dc165e3859dc4118ed
marcondercin <marcondercin@gmail.com>
2021-09-02 21:20:43 -0700

    search method

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,5 @@
+const clientID = '0c5eccee3a6b4a9db7d88cd8652c87bd';
+const redirectURI = 'http://localhost:3000';
 let accessToken;
 
 const Spotify = {
@@ -16,8 +18,34 @@ const Spotify = {
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
       window.history.pushState('Access Token', null, '/');
       return accessToken;
+    } else {
+      const accessURL = `https://accounts.spotify.com/authorize?client_id=${clientID}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+      window.location = accessURL;
     }
+  },
+
+  search(term) {
+    const accessToken = Spotify.getAccessToken();
+    return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`
+      }
     }
+    ).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (!jsonResponse.tracks) {
+        return [];
+      }
+      return jsonResponse.tracks.items.map(track => ({
+        id: track.id,
+        name: track.name,
+        artist: track.artists[0].name,
+        album: track.album.name,
+        uri: track.uri
+      }));
+    })
+  }
   }
 
 export default Spotify;
